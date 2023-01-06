24a8d63b12cf2d025adaa7b538d07495a2c00018
02-131212-005 <02-131212-005@student.bahria.edu.pk>
2023-01-06 14:25:29 +0500

    something

---
+++ Controllers/NewRegController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Reg_User.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+
+
+namespace MVC_Reg_User.Controllers
+{
+    public class NewRegController : Controller
+    {
+        // GET: NewReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserClass uc,HttpPostedFileBase file)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery= "insert into [dbo].[MVCregUser] (Uname,Uemail,Upwd,Gender,Uimg) values(@Uname,@Uemail,@Upwd,@Gender,@Uimg)";
+            SqlCommand sqlcom = new SqlCommand(sqlquery, sqlconn);
+            sqlconn.Open();
+            sqlcom.Parameters.AddWithValue("@Uname", uc.Uname);
+            sqlcom.Parameters.AddWithValue("Uemail", uc.Uemail);
+            sqlcom.Parameters.AddWithValue("@Upwd", uc.Upwd);
+            sqlcom.Parameters.AddWithValue("@Gender", uc.Gender);
+            if(file!=null && file.ContentLength>0)
+                {
+                string filename = Path.GetFileName(file.FileName);
+                string imgpath = Path.Combine(Server.MapPath("~/User-Images/"), filename);
+                file.SaveAs(imgpath);
+
+            }
+            sqlcom.Parameters.AddWithValue("@Uimg", "~/User-Images/" + file.FileName);
+            sqlcom.ExecuteNonQuery();
+            sqlconn.Close();
+            ViewData["Message"] = "User Record" + uc.Uname + "is saved successfully !";
+
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/UserClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Reg_User.Models
+{
+    public class UserClass
+    {
+        [Required(ErrorMessage ="Enter username  !")]
+        [Display(Name ="Enter name:")]
+        [StringLength(maximumLength:7 , MinimumLength =3,ErrorMessage ="username length must be max 7 & min 3")]
+        public string Uname { get; set; }
+
+        [Required(ErrorMessage = "please Enter email  !")]
+        [Display(Name = "Email Id:")]
+        public string Uemail { get; set; }
+
+        [Required(ErrorMessage = "Enter the password  !")]
+        [Display(Name = "password:")]
+        [DataType(DataType.Password)]
+        public string Upwd { get; set; }
+
+        [Required(ErrorMessage = "Enter the password  !")]
+        [Display(Name = "Re-password:")]
+        [DataType(DataType.Password)]
+        [Compare("Upwd")]
+        public string Repwd { get; set; }
+
+
+        [Required(ErrorMessage = "select the gender  !")]
+        [Display(Name = "Gender:")]
+        public char Gender { get; set; }
+
+
+        [Required(ErrorMessage = "Upload Profile Image !")]
+        [Display(Name = "profile Image:")]
+        public string Uimg { get; set; }
+    }
+}
